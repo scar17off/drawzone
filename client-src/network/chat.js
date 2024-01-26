5b2784d37f76ef35a1186d770c63f6c46c769b04
scar17off <scar17off@gmail.com>
2024-01-27 00:37:48 +0200

    chat design

--- client-src/network/chat.js
+++ client-src/network/chat.js
@@ -1,10 +1,29 @@
 import socket from "./network.js";
 
-export default {
+const chat = {
     send: function(message) {
         socket.emit("send", message);
+        document.getElementById("chat-messages").insertAdjacentHTML("beforeend", 
+`<li class="user">
+    <span>${message}</span>
+</li>`);
     },
     local: function(message) {
         console.log(message);
+        document.getElementById("chat-messages").insertAdjacentHTML("beforeend", 
+`<li class="user">
+    <span>${message}</span>
+</li>`);
     }
 }
+
+document.getElementById("chat-message").addEventListener("keydown", event => {
+    if(event.key == "Enter") {
+        const message = document.getElementById("chat-message").value;
+        if(message.value == '') return;
+        chat.send(message);
+        document.getElementById("chat-message").value = '';
+    }
+})
+
+export default chat;
