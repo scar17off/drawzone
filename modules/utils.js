a10df2261ba15bd0430a1dd1c34638c5ef36900f
scar17off <scar17off@gmail.com>
2024-05-26 02:25:37 +0300

    Expanded documentation with additional JsDoc comments

--- modules/utils.js
+++ modules/utils.js
@@ -1,14 +1,32 @@
 const chalk = require("chalk");
 
+/**
+ * Extends Date prototype to format the current date as DD-MM-YYYY.
+ * @returns {string} - The formatted date string.
+ */
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"-"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"-"+ this.getFullYear();
 }
+
+/**
+ * Extends Date prototype to format the current time as HH:MM:SS.
+ * @returns {string} - The formatted time string.
+ */
 Date.prototype.timeNow = function () {
     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 }
 
+/**
+ * Sanitizes input to prevent XSS attacks by escaping HTML special characters.
+ * @param {string} input - The string to sanitize.
+ * @returns {string} - The sanitized string.
+ */
 const sanitizeXSS = input => !input ? input : input.replace(/[&<>"'/]/g, (match) => ({'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#x27;', '/': '&#x2F;'})[match] || match);
-
+/**
+ * Converts seconds to a formatted string.
+ * @param {number} seconds - The number of seconds to convert.
+ * @returns {string} - The formatted string.
+ */
 function convertTime(seconds) {
     let minutes = Math.floor(seconds / 60);
     let sec = Math.floor(seconds % 60);
